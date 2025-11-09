aa761daef987de93b28d2d9e0dde17a470aea3de
niallivmi <majucavalcantix@gmail.com>
2025-11-09 15:10:24 -0300

    Projeto Ininicial

---
+++ main.js
@@ -0,0 +1,15 @@
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play()
+}
+
+let contador  = 0
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom
+
+    contador = contador + 1
+
+    console.log(contador)
+}
