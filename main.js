3f42f9e3c83540e8de587eba00beebdab3200a62
niallivmi <majucavalcantix@gmail.com>
2025-11-16 18:47:55 -0300

    Alterar while para for.

--- main.js
+++ main.js
@@ -4,22 +4,14 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play()
 }
 
-let contador  = 0
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
  
     const tecla = listaDeTeclas[contador]
     const instrumento = listaDeTeclas[contador].classList[1]
-
     const idAudio =  `#som_${instrumento}`
 
-    console.log(idAudio)
-
-    listaDeTeclas[contador].onclick = function ( ) {
-        tocaSom(idAudio)
+    tecla.onclick = function () {
+            tocaSom(idAudio)
     }
-
-    contador = contador + 1
-
-    console.log(contador)
+   
 }
